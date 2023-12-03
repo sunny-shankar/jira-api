1fae5b8c458274e8f88a37e549460c91d53029da
Sunny Shankar <sunny.shankar44@gmail.com>
2023-12-03 19:40:57 +0530

    added-test-case

--- src/jira/jira.controller.spec.ts
+++ src/jira/jira.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JiraController } from './jira.controller';
+import { JiraService } from './jira.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 describe('JiraController', () => {
   let controller: JiraController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), HttpModule],
       controllers: [JiraController],
+      providers: [JiraService],
     }).compile();
 
     controller = module.get<JiraController>(JiraController);
@@ -14,5 +19,11 @@ describe('JiraController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be get jira statics', async () => {
+    const response = await controller.getIssueStatistics();
+    expect(response.success).toBe(true);
+    expect(response.code).toBe('SUCCESS');
   });
 });
--- src/jira/jira.service.spec.ts
+++ src/jira/jira.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JiraService } from './jira.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 describe('JiraService', () => {
   let service: JiraService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), HttpModule],
       providers: [JiraService],
     }).compile();
 
@@ -14,5 +17,21 @@ describe('JiraService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get assigned issue', async () => {
+    const response = await service.getAssignedIssue();
+    expect(response.total).toBeGreaterThan(0);
+  });
+
+  it('should get unassigned issue', async () => {
+    const response = await service.getUnassignedIssue();
+    expect(response.total).toBeGreaterThan(0);
+  });
+
+  it('should get unassigned issue - random auth token', async () => {
+    process.env['JIRA_AUTH_TOKEN'] = '1';
+    const response = await service.getAssignedIssue();
+    expect(response).toHaveProperty('errorMessages');
   });
 });
