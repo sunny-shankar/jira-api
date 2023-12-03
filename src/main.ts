d758d8bada09c8ed53dd0973ba2fdc1ef753e982
Sunny Shankar <sunny.shankar44@gmail.com>
2023-12-03 14:46:24 +0530

    cors-updated

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.enableCors();
   app.useLogger(app.get(Logger));
   await app.listen(process.env.PORT || 3000);
 }
